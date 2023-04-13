46c1754823bf3f674f0ef5fd4b5459ce8f5fbf07
EveryoneStandUp <wjsgudwns98@naver.com>
2023-04-13 14:51:21 +0900

    sql

---
+++ src/main/webapp/WEB-INF/sql/01select.sql
@@ -0,0 +1,35 @@
+-- SELECT
+-- 데이터 읽기(가져오기)
+
+-- table (행, 열)
+-- 행(row, record)
+-- 열(column, field, attibute)
+
+-- SELECT 열 목록
+-- FROM 테이블 명
+-- 쿼리 끝에 ; 작성
+
+-- Customers 테이블에서 모든 레코드(행)와 모든 필드(열)을 가져와라(읽어라) 
+SELECT * FROM Customers; -- 실행 ctrl + enter
+
+-- 문제1) Employees 테이블에서
+-- 모든 행과 모든 열을 읽기
+SELECT * FROM Employees;
+SELECT * FROM Suppliers;
+SELECT * FROM Categories;
+SELECT * FROM OrderDetails;
+SELECT * FROM Orders;
+SELECT * FROM Products;
+select * from Products;
+
+-- 작성 관습
+-- sql은 대소문자 구분안함()
+-- 키워드는 대문자 / 테이블명, 컬렴명은 소문자
+
+
+
+
+
+
+
+
