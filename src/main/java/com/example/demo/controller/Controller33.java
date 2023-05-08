2f85aeccae9ae1ddebe62e7cb88c5b762a19024c
EveryoneStandUp <wjsgudwns98@naver.com>
2023-05-08 16:11:24 +0900

    boot

---
+++ src/main/java/com/example/demo/controller/Controller33.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("sub33")
+public class Controller33 {
+
+	@GetMapping("mylogin")
+	public void loginForm() {
+		
+	}
+}
